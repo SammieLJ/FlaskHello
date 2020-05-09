4df2ec66b7b8110a1adba6ba603421529c9255fa
SammieLJ <samir.subasic@gmail.com>
2020-05-09 14:43:25 +0200

    Changed to more dynamic example
    Cleaned code that is not needed for Heroku deployment.

---
+++ main.py
@@ -0,0 +1,31 @@
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/")
+def index():
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+
+@app.route("/about-me")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    # app.run(port=5000)
+    app.run(debug=True)  # if you use the port parameter, delete it before deploying to Heroku
+
